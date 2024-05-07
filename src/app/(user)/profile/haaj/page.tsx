94692c4dab9c889e76905ea98a018054f7cecede
merzouka <marzoukayouness@gmail.com>
2024-05-07 22:27:51 +0100

    added back revalidation, fixed small bug

--- src/app/(user)/profile/haaj/page.tsx
+++ src/app/(user)/profile/haaj/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { useUser } from "@/hooks/use-user";
 import { Pages } from "@/constants/pages";
 import { cn } from "@/lib/utils";
 import { ProfileForm } from "@/app/components/profile-form";
 
 export default function ProfilePage() {
-    const { validateAccess } = useUser();
-    // validateAccess(Pages.profile);
     return (
         <div className="w-full h-full relative">
             <div className="p-5 w-full h-full overflow-scroll absolute top-0 right-0 left-0 bottom-0">
