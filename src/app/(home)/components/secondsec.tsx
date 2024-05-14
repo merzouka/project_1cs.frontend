e5c7a320acf130b49ff334d418c50e333863b624
merzouka <marzoukayouness@gmail.com>
2024-05-14 12:11:22 +0100

    Changes: - fixed styling - finished home page

--- src/app/(home)/components/secondsec.tsx
+++ src/app/(home)/components/secondsec.tsx
@@ -3,137 +3,6 @@ import Link from "next/link";
 import { Scroller } from "./scroller";
 import { IoMdArrowDropright } from "react-icons/io";
 
-/* 
- export default function SecondSec() {
-
-    const plugin = React.useRef(
-        Autoplay({ delay: 2000, stopOnInteraction: true })
-    )
-
-
-    return (
-        <>
-            <div className=" mt-[20px] flex justify-center text-center mb-3">
-                <h1 className="font-bold text-4xl font-custom drop-shadow-md  ">Découvrez nos services</h1>
-            </div>
-            <div className="flex justify-center">
-                <hr className="mb-4 mx-auto my-2 h-0.5 w-[150px] border-0 bg-[#EBA565] " />
-            </div>
-            <div className=" mt-[5px] flex justify-center text-center  text-[#8B929D] mb-10" >
-                Cette plateforme a été créée pour faciliter les services du Hajj aux citoyens,
-                <br></br>
-                où vous pouvez via cette plateforme :
-            </div>
-
-            <Carousel
-                className="w-full  mb-[100px]"
-                plugins={[plugin.current]}
-                onMouseEnter={plugin.current.stop}
-                onMouseLeave={plugin.current.reset}
-            >
-                <CarouselContent className="" >
-                    {Array.from({ length: 1 }).map((_, index) => (
-                        <CarouselItem key={index} className="flex space-x-5 ">
-
-                            <div className=' ml-[100px] h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px] '  >
-                                    <p className='text-xl font-bold ' >
-                                        Inscription au tirage
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat
-                                    </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-
-                            <div className=' h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg  w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px]'>
-                                    <p className='text-xl font-bold ' >
-                                        Payement des frais
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat                                </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More  &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-                            <div className=' h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px]'>
-                                    <p className='text-xl font-bold ' >
-                                        Payement des frais
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat                                </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </CarouselItem>))}
-                    {Array.from({ length: 1 }).map((_, index) => (
-                        <CarouselItem key={index} className="flex space-x-5 ">
-
-                            <div className=' ml-[100px] h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px] '  >
-                                    <p className='text-xl font-bold ' >
-                                        Flights
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat
-                                    </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-
-                            <div className=' h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg  w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px]'>
-                                    <p className='text-xl font-bold ' >
-                                        Hotel
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat                                </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More  &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-                            <div className=' h-[200px]  rounded-[50px] bg-white  mb-5 shadow-lg w-[320px]' >
-                                <div className='flex flex-col justify-center  gap-4 p-4 w-[300px]'>
-                                    <p className='text-xl font-bold ' >
-                                        Guide
-                                    </p>
-                                    <p className='text-[#8B929D]'>
-                                        Vous pouvez vous inscrire au tirage et recevoir un SMS pour connaître la résultat                                </p>
-                                    <button className='text-[#EBA565] mr-[140px] text-start ml-3 '>
-                                        Learn More &nbsp;  &gt;
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </CarouselItem>))}
-
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
-        </>)
-
-
-}
- * */
 
 export default function SecondSec() {
     const elements = useMemo(() => services.map(service => ({
@@ -154,7 +23,7 @@ export default function SecondSec() {
                 <br></br>
                 où vous pouvez via cette plateforme :
             </div>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mb-40">
                 <Scroller elements={elements} perPage={3}/>
             </div>
         </>
@@ -203,13 +72,13 @@ const services: Service[] = [
 
 const ServiceCard = ({ service }: { service: Service }) => {
     return (
-        <div key={service.id} className=' h-[200px] rounded-3xl bg-white drop-shadow-xl w-[320px] px-8 py-4 flex flex-col relative'>
-            <div className='w-full h-full flex flex-col justify-between '>
+        <div key={service.id} className=' h-[200px] rounded-3xl bg-white drop-shadow-xl w-[320px] px-8 py-4 flex flex-col relative group pb-6'>
+            <div className='w-full h-full flex flex-col justify-between'>
                 <h3 className='text-xl font-bold text-wrap'>
                     {service.title}
                 </h3>
-                <p className='text-slate-500'>{service.description}</p>
-                <Link href={service.link} className='w-fit text-orange-300 font-bold flex items-center gap-x-2'>
+                <p className='text-slate-500 group-hover:text-slate-700'>{service.description}</p>
+                <Link href={service.link} className='w-fit text-orange-300 group-hover:text-orange-400 font-bold flex items-center gap-x-2'>
                     {"Apprendre Plus"}
                     <IoMdArrowDropright className="size-6 p-0"/>
                 </Link>
