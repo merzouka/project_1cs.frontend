889cb53825e4c81aec4c8a721150e60b870b2a4f
merzouka <marzoukayouness@gmail.com>
2024-05-23 23:34:03 +0100

    Changes: - restored inscription pages

---
+++ src/app/(user)/(inscription)/inscription/Secpage/page.tsx
@@ -0,0 +1,27 @@
+
+"use client"
+import React from 'react'
+
+import NavBar from '../../components/NavBar'
+import Title from '../../components/Titlee'
+import InscriptionPage2 from '../../components/Secform'
+
+export default function Page2() {
+    return (
+        <>
+            <div
+                className="bg-fixed bg-center bg-cover h-screen overflow-x-hidden "
+                style={{
+                    backgroundImage: `url('/3.jpg')`,
+                }}
+            >
+                <NavBar />
+                <Title />
+                <InscriptionPage2 />
+            </div>
+        </>
+
+
+    )
+}
+
