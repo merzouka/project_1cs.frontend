97d2404670b7ca452f482edb01ce0bc35f49c95a
merzouka <marzoukayouness@gmail.com>
2024-05-30 21:13:06 +0100

    Changes: - removed (roles) copy of code

--- src/app/(admin)/(roles)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { DrawingNavBar } from "@/app/(admin)/(drawing)/components/nav-bar";
-import { ManagerLayout } from "@/app/(admin)/components/manager-layout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ManagerLayout navBar={<DrawingNavBar />}>{children}</ManagerLayout>;
-}
