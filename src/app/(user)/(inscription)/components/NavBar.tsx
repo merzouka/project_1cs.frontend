4c11fd4952bd23c781ea890eecde3cc60465898c
firdaws <firdawsbenahmed20@gmail.com>
2024-06-07 14:03:22 +0000

    h

--- src/app/(user)/(inscription)/components/NavBar.tsx
+++ src/app/(user)/(inscription)/components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
   return (
     <>
       <nav className=" w-full h-12 z-15 ml-20 mt-2">
-        <Link href={"/home"}>
+        <Link href={"/"}>
           <Image alt="Logo" src="/logo.svg" width={58} height={90} />
         </Link>
 
